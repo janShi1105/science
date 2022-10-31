b7a7ae288e577d825055fa6f72891ec3a41f6e20
公務員のかっちゃん <a.k.rem.sunao@gmail.com>
2022-11-01 08:42:53 +0900

    傾向スコアの正体を掴みきれていない不安がある

--- RStudio/basicStatics/R/vounchers.R
+++ RStudio/basicStatics/R/vounchers.R
@@ -36,10 +36,55 @@ using_voucher_results <- df_results %>%
 
 data_tbl4_bog95 <- vouchers %>%
   filter(BOG95SMP == 1, TAB3SMPL == 1,
-         !is.na(SCYFNSH), !is.na(FINISH6), !is.na(PRSCHA_1), !is.na(RERT6), !is.na(NREPT), !is.na(INSCHL),
+         !is.na(SCYFNSH), !is.na(FINISH6), !is.na(PRSCHA_1), !is.na(REPT6), !is.na(NREPT), !is.na(INSCHL),
          !is.na(FINISH7), !is.na(PRSCH_C), !is.na(FINISH8), !is.na(PRSCHA_2), !is.na(TOTSCYRS), !is.na(REPT)) %>%
   select(VOUCH0, SVY, HSVISIT, DJAMUNDI, PHONE, AGE, STRATA1:STRATA6, STRATAMS, DBOGOTA, D1993, D1995, D1997,
          DMONTH1:DMONTH12, SEX_MISS, FINISH6, FINISH7, FINISH8, REPT6, REPT, NREPT, SEX2, TOTSCYRS, MARRIED,
          HASCHILD, HOURSUM, WORKING3, INSCHL, PRSCH_C, USNGSCH, PRSCHA_1)
 
+regression_data <- data_tbl4_bog95 %>% filter(SEX2 == 0)
 
+df_models <- models %>%
+  mutate(model = map(.x = formula, .f = lm, data = regression_data)) %>%
+  mutate(lm_result = map(.x = model, .f = tidy))
+
+df_results_female <- df_models %>%
+  mutate(formula = as.character(formula), gender='female') %>%
+  select(formula, model_index, lm_result, gender) %>%
+  unnest(cols = c(lm_result))
+
+regression_data <- data_tbl4_bog95 %>% filter(SEX2 == 1)
+
+df_models <- models %>%
+  mutate(model = map(.x = formula, .f = lm, data = regression_data)) %>%
+  mutate(lm_result = map(.x = model, .f = tidy))
+
+df_results_male <- df_models %>%
+  mutate(formula = as.character(formula), gender = 'male') %>%
+  select(formula, model_index, lm_result, gender) %>%
+  unnest(cols = c(lm_result))
+
+ps_model <- glm(data =biased_data,
+                formula = treatment ~ recency + history + channel,
+                family = binomial)
+
+m_near <- matchit(formula = treatment ~ recency + history + channel,
+                  data = biased_data,
+                  method = 'nearest',
+                  replace = TRUE)
+
+matched_data <- match.data(m_near)
+
+PSM_result <- matched_data %>%
+  lm(spend ~ treatment, data = .) %>%
+  tidy()
+
+weighting <- weightit(formula = treatment ~ recency + history + channel,
+                      data = biased_data,
+                      method = 'ps',
+                      estimand ='ATE')
+
+IPW_result <- lm(data = biased_data,
+                 formula = spend ~ treatment ,
+                 weights = weighting$weights) %>%
+  tidy()
